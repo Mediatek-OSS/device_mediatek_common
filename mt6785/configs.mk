a862a832c4a423ed71ff84dd10f63a6e8e99209b
Zinadin Zidan <zidan44@pixelexperience.org>
2021-06-01 10:18:32 +0600

    device: mtk: Build power.mtxx for mtk platforms
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ic6ca9acb6fe279c2fd6aa546147760734b58a9b5

--- mt6785/configs.mk
+++ mt6785/configs.mk
@@ -3,3 +3,7 @@ $(call inherit-product, device/mediatek/common/mt6785/ProjectConfig.mk)
 
 # Call MTK Opensource Repo
 $(call inherit-product, vendor/mediatek/opensource/mtk-builds.mk)
+
+# Power
+PRODUCT_PACKAGES += \
+    power.mt6785
