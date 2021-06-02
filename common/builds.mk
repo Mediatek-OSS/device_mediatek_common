4a937450b50f4262306f6c3647076773e4e077ba
Zinadin Zidan <zidan44@pixelexperience.org>
2021-06-02 07:21:04 +0600

    device: mtk: Import common mtk kernel headers
    * Its required by some mtk hals.
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ia157297131b6f247f2bad1a8b4a8df8ffe02e3ab

---
+++ common/builds.mk
@@ -0,0 +1,4 @@
+MTK_DEVICE_PATH := device/mediatek/common
+
+# Kernel Headers (MTK)
+PRODUCT_VENDOR_KERNEL_HEADERS := $(MTK_DEVICE_PATH)/common/kernel-headers
--- mt6763/configs.mk
+++ mt6763/configs.mk
@@ -4,6 +4,9 @@ $(call inherit-product, device/mediatek/common/mt6763/ProjectConfig.mk)
 # Call MTK Opensource Repo
 $(call inherit-product, vendor/mediatek/opensource/mtk-builds.mk)
 
+# Call common mtk builds
+$(call inherit-product, device/mediatek/common/common/builds.mk)
+
 # Consumerir
 PRODUCT_PACKAGES += \
     consumerir.mt6763
