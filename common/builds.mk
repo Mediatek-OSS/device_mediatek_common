c96cd47289f8a4a208490b4ac8ca3e5271f82e29
Zinadin Zidan <zidan44@pixelexperience.org>
2021-06-02 07:42:42 +0600

    device: mtk: common: Copy mtk stock apn configs
    * From begonia-user-11-RP1A.200720.011-21.5.25-release-keys
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I3f5c2c88e7b1c350f0c9b9e1b2d55962e6fff8b2

--- common/builds.mk
+++ common/builds.mk
@@ -2,3 +2,7 @@ MTK_DEVICE_PATH := device/mediatek/common
 
 # Kernel Headers (MTK)
 PRODUCT_VENDOR_KERNEL_HEADERS := $(MTK_DEVICE_PATH)/common/kernel-headers
+
+# APN
+PRODUCT_COPY_FILES += \
+    $(MTK_DEVICE_PATH)/common/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
