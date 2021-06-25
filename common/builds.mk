dc5095a660f7c2740988aded569deba3a6470cf8
Zinadin Zidan <zidan44@pixelexperience.org>
2021-06-25 11:05:11 +0600

    device: mtk: common: Import mtk privapp permissions
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: If2dd2dabb1a5ef373cc9be92a42ad3fa0dbaa7a9

--- common/builds.mk
+++ common/builds.mk
@@ -6,3 +6,7 @@ PRODUCT_VENDOR_KERNEL_HEADERS := $(MTK_DEVICE_PATH)/common/kernel-headers
 # APN
 PRODUCT_COPY_FILES += \
     $(MTK_DEVICE_PATH)/common/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
+# Permissions MTK
+PRODUCT_COPY_FILES += \
+    $(MTK_DEVICE_PATH)/common/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
