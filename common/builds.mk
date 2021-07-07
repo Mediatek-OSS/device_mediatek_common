316fe5aaa7c02762b4ce38172e2a086c36b9252c
Gagan Malvi <malvigagan@gmail.com>
2021-07-07 14:27:41 +0600

    device: mtk: common: seccomp: Import mtk seccomp policy
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I64421b79fba2670320d3b70e4650ff44e9c13495

--- common/builds.mk
+++ common/builds.mk
@@ -12,3 +12,9 @@ PRODUCT_COPY_FILES += \
     $(MTK_DEVICE_PATH)/common/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
     $(MTK_DEVICE_PATH)/common/permissions/product-permissions-mediatek.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-mediatek.xml \
     $(MTK_DEVICE_PATH)/common/permissions/system-ext-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/system-ext-permissions-mediatek.xml
+
+# Seccomp Policy
+PRODUCT_COPY_FILES += \
+     $(MTK_DEVICE_PATH)/common/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+     $(MTK_DEVICE_PATH)/common/seccomp/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy \
+     $(MTK_DEVICE_PATH)/common/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
